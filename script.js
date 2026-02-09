3b4e0c3466473a140ae5c1f70c58753b6847ce9d
amakeetile <najaneakeetile@gmail.com>
2026-02-09 15:13:06 +0200

    Add interactive buttons for Valentine's Day response

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const response = document.getElementById("response");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+  response.innerHTML = `
+    💖 I had a feeling.<br><br>
+    I wish I was there instead of doing this through a screen,<br>
+    but this is me choosing you anyway.<br><br>
+    Happy Valentine’s Day 🫶
+  `;
+});
 
